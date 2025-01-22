41915f4390d1aed5007c777d15bd6965c723d380
Woozae <151023847+Woozae@users.noreply.github.com>
2025-01-22 20:02:05 +0900

    feat:북마크 페이지

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import "./LoginPage.css";
-import LoginPage from "./LoginPage";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<LoginPage />);
-
+root.render(<App />);
